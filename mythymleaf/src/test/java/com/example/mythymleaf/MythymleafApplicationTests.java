697ba716ada3c12cbbfd883aa1f886c8fba3cf4f
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-09-26 22:48:42 +0900

    layout 구성

---
+++ mythymleaf/src/test/java/com/example/mythymleaf/MythymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

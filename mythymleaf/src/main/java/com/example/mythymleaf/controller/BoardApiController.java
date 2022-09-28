993fbe59a1de4f29f2f6e71f10d6f924c0e393f1
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-09-28 13:58:17 +0900

    practice api

---
+++ mythymleaf/src/main/java/com/example/mythymleaf/controller/BoardApiController.java
@@ -0,0 +1,60 @@
+package com.example.mythymleaf.controller;
+
+import com.example.mythymleaf.model.Board;
+import com.example.mythymleaf.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if (StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return repository.findAll();
+        } else {
+            return repository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(Board -> {
+                    Board.setTitle(newBoard.getTitle());
+                    Board.setContent(newBoard.getContent());
+                    return repository.save(Board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
--- mythymleaf/src/main/java/com/example/mythymleaf/repository/BoardRepository.java
+++ mythymleaf/src/main/java/com/example/mythymleaf/repository/BoardRepository.java
@@ -3,5 +3,11 @@ package com.example.mythymleaf.repository;
 import com.example.mythymleaf.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+
+    List<Board> findByTitleOrContent(String title, String content);
 }
